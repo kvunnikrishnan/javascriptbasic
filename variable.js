693c583249e5ce510b5e89ca07641711830459c4
UNNI <kv.unnikrishnan89@gmail.com>
2020-02-12 13:37:11 +0530

    assignment

--- variable.js
+++ variable.js
@@ -12,3 +12,12 @@ console.log(name);
 console.log(status);
 console.log(hello);
 
+
+//constants 
+//Here constants cannot be renamed. attempt to rename will throw error
+const mybirthday='25-08-1989'
+mybirthday='20-03-1990'
+
+console.log(mybirthday)
+
+
---
+++ variablePractice1.js
@@ -0,0 +1,6 @@
+let admin;
+let name;
+name='john';
+admin=name;
+console.log(admin)
+console.log(name);
